b67e33049e047b357e71054c31f6fd82da997163
Marcelo Ortiz Peredo <a2018119133@estudiantes.upsa.edu.bo>
2021-02-25 20:04:15 -0400

    Se creo calculator controller y se agrego suma,resta,multiplicacion y division

---
+++ SolCalculadora/ApiCalculadora/Controllers/CalculatorControllers.cs
@@ -0,0 +1,37 @@
+﻿namespace ApiCalculadora.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorControllers : ControllerBase
+    {
+        [HttpPost]
+        [Route("Add")]
+        public int Add([FromHeader] int numa, [FromHeader] int numb)
+        {
+            return numa + numb;
+        }
+
+        [HttpPost]
+        [Route("Multiply")]
+        public int Multiply([FromHeader] int numa, [FromHeader] int numb)
+        {
+            return numa * numb;
+        }
+
+        [HttpPost]
+        [Route("Divide")]
+        public int Divide([FromHeader] int numa, [FromHeader] int numb)
+        {
+            return numa / numb;
+        }
+
+        [HttpPost]
+        [Route("Substract")]
+        public int Substract([FromHeader] int numa, [FromHeader] int numb)
+        {
+            return numa - numb;
+        }
+    }
+}
